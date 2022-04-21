8f8bb01e37f5ac279551444177b9bbde15a192c4
SuraZz007 <surajdev9886@gmail.com>
2022-04-21 13:47:16 +0530

    array and funciton assessment

--- script.js
+++ script.js
@@ -142,3 +142,12 @@
 // if (friends.includes("Ram")) {
 //   console.log(`Jay Shree Ram`);
 // }
+const calcTip = (bill) => {
+  return bill >= 50 && bill >= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
