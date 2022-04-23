ed65df57490cfcf90c1a36de687fccbb6ee25367
SuraZz007 <surajdev9886@gmail.com>
2022-04-23 21:26:57 +0530

    for loop on arrays

--- script.js
+++ script.js
@@ -200,33 +200,56 @@
 //     );
 // BMI = Mass / Height * Height
 
-const mark = {
-  firstName: "Mark",
-  lastName: "Miller",
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    return this.mass / (this.height * this.height);
-  },
-};
-const john = {
-  firstName: "John",
-  lastName: "Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    return this.mass / (this.height * this.height);
-  },
-};
+// const mark = {
+//   firstName: "Mark",
+//   lastName: "Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     return this.mass / (this.height * this.height);
+//   },
+// };
+// const john = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     return this.mass / (this.height * this.height);
+//   },
+// };
 
-john.calcBMI > mark.calcBMI
-  ? console.log(
-      `${john.firstName}'s BMI (${john.calcBMI()}) is higher than ${
-        mark.firstName
-      }'s BMI (${mark.calcBMI()}) ! `
-    )
-  : console.log(
-      `${mark.firstName}'s BMI (${mark.calcBMI()}) is higher than ${
-        john.firstName
-      }'s BMI (${john.calcBMI()}) ! `
-    );
+// john.calcBMI > mark.calcBMI
+//   ? console.log(
+//       `${john.firstName}'s BMI (${john.calcBMI()}) is higher than ${
+//         mark.firstName
+//       }'s BMI (${mark.calcBMI()}) ! `
+//     )
+//   : console.log(
+//       `${mark.firstName}'s BMI (${mark.calcBMI()}) is higher than ${
+//         john.firstName
+//       }'s BMI (${john.calcBMI()}) ! `
+//     );
+
+// for (let i = 1; i <= 10; ) {
+//   console.log(i);
+//   i++;
+// }
+
+// for (let rep = 1; rep <= 10; ) {
+//   console.log(`Lifting weights ${rep}`);
+//   rep++;
+// }
+
+const jonas = [
+  "Jonas",
+  "Schmdn",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+}
