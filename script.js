c1ef07a2cb6ff8cebbb74f9cf3a2377cfea57563
SuraZz007 <surajdev9886@gmail.com>
2022-04-24 14:54:30 +0530

    loop inside a loop

--- script.js
+++ script.js
@@ -282,13 +282,21 @@
 
 //   console.log(jonas[i], typeof jonas);
 // }
-const jonas = [
-  "Jonas",
-  "Schmdn",
-  2037 - 1991,
-  "teacher",
-  ["Michael", "Peter", "Steven"],
-];
-for (let i = jonas.length - 1; i >= 0; i--) {
-  console.log(jonas[i]);
+// const jonas = [
+//   "Jonas",
+//   "Schmdn",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+// ];
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//   console.log(jonas[i]);
+// }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weight repetition ${rep}`);
+  }
 }
