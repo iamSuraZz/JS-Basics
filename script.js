9f0ed900b1502932414266f10d2e4ec5f0cb14b6
SuraZz007 <surajdev9886@gmail.com>
2022-05-23 16:36:33 +0530

    DOM manipulation and css property

--- script.js
+++ script.js
@@ -379,16 +379,27 @@
 
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   console.log(guess, typeof guess);
+
+  //When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "No number selected 🚫";
+
+    //If player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "✌️ Correct Numer ";
+
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = " ⬆️ Too high";
@@ -398,6 +409,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "😭 You lost the Game";
       document.querySelector(".score").textContent = 0;
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = " ⬇️ Too low";
