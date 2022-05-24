6a78124571d0672992f1281bb33feb53a20da37b
SuraZz007 <surajdev9886@gmail.com>
2022-05-24 19:13:51 +0530

    make basic structure for dice game

--- script.js
+++ script.js
@@ -460,3 +460,24 @@
 //     closeModal();
 //   }
 // });
+
+//Selecting elements
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+//Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  //1. Generating a rondom dice roll
+  //2. Display dice
+  //3. Check for rolled 1: if true, switch to next player
+});
