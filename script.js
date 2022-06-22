f4938feddd6f0547183013e860f54e4fea77a1f2
SuraZz007 <surajdev9886@gmail.com>
2022-06-22 17:48:48 +0530

    Default parameters in JavaScript

--- script.js
+++ script.js
@@ -1466,16 +1466,41 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 // planesInLine(3);
 // planesInLine(12);
 
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-const getCode = (str) => str.slice(0, 3).toUpperCase();
+// const getCode = (str) => str.slice(0, 3).toUpperCase();
 
-for (const flight of flights.split("+")) {
-  const [type, from, to, time] = flight.split(";");
-  const output = `${type.startsWith("_Delayed") ? "🛑" : ""}${type.replaceAll(
-    "_",
-    " "
-  )} ${getCode(from)} ${getCode(to)}( ${time.replace(":", "h")})`.padStart(36);
-  console.log(output);
-}
+// for (const flight of flights.split("+")) {
+//   const [type, from, to, time] = flight.split(";");
+//   const output = `${type.startsWith("_Delayed") ? "🛑" : ""}${type.replaceAll(
+//     "_",
+//     " "
+//   )} ${getCode(from)} ${getCode(to)}( ${time.replace(":", "h")})`.padStart(36);
+//   console.log(output);
+// }
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+createBooking("LH123", undefined, 1000);
