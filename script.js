cc4ff6f927c50cbf836988e64f0ca5737658d649
SuraZz007 <surajdev9886@gmail.com>
2022-06-25 13:42:46 +0530

    Functions accepting callback functions

--- script.js
+++ script.js
@@ -1480,57 +1480,86 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 //   console.log(output);
 // }
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
-createBooking("LH123", undefined, 1000);
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking("LH123");
+// createBooking("LH123", 2, 800);
+// createBooking("LH123", 2);
+// createBooking("LH123", 5);
+// createBooking("LH123", undefined, 1000);
 
-const flight = "LH243";
-const suraj = {
-  name: "Suraj Poddar",
-  passport: 345678876,
-};
+// const flight = "LH243";
+// const suraj = {
+//   name: "Suraj Poddar",
+//   passport: 345678876,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = "LH999";
-  passenger.name = "Mr." + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LH999";
+//   passenger.name = "Mr." + passenger.name;
 
-  if (passenger.passport === 345678876) {
-    alert("Check in");
-  } else {
-    alert("wrong passport");
-  }
-};
+//   if (passenger.passport === 345678876) {
+//     alert("Check in");
+//   } else {
+//     alert("wrong passport");
+//   }
+// };
+// // checkIn(flight, suraj);
+// // console.log(flight);
+// // console.log(suraj);
+
+// // Is same as doing...
+// // const flightNum = flight;
+// // const passenger = suraj;
+
+// const newPassport = function (person) {
+//   person.passport = Math.floor(Math.random() * 10000000000);
+// };
+// newPassport(suraj);
 // checkIn(flight, suraj);
-// console.log(flight);
-// console.log(suraj);
 
-// Is same as doing...
-// const flightNum = flight;
-// const passenger = suraj;
-
-const newPassport = function (person) {
-  person.passport = Math.floor(Math.random() * 10000000000);
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
 };
-newPassport(suraj);
-checkIn(flight, suraj);
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log("🙏🙏");
+};
+
+document.body.addEventListener("click", high5);
+
+["Suraj", "Deepak", "Keshav", "Anshu"].forEach(high5);
