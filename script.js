86262c9603f8687b9b041e55668d6b503122c1e1
SuraZz007 <surajdev9886@gmail.com>
2022-06-26 13:20:56 +0530

    Immidiately Invoked Functions Expressions (IIFE)

--- script.js
+++ script.js
@@ -1674,40 +1674,55 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 // console.log(addVAT2(100));
 // console.log(addVAT2(23));
 
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0 : JavaScript", "1 : Python", "2 : Rust", "3 : C++"],
-  // This generates [0,0,0,0].
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
-      )
-    );
-    console.log(answer);
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0 : JavaScript", "1 : Python", "2 : Rust", "3 : C++"],
+//   // This generates [0,0,0,0].
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+//       )
+//     );
+//     console.log(answer);
 
-    // Register the answer
-    typeof answer === "number" &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
+//     // Register the answer
+//     typeof answer === "number" &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
 
-    this.displayResults();
-    this.displayResults("string");
-  },
-  displayResults(type = "array") {
-    if (type === "array") {
-      console.log(this.answers);
-    } else if (type === "string") {
-      // Poll results are 13, 2, 4,1
-      console.log(`Poll results are ${this.answers.join(", ")}`);
-    }
-  },
+//     this.displayResults();
+//     this.displayResults("string");
+//   },
+//   displayResults(type = "array") {
+//     if (type === "array") {
+//       console.log(this.answers);
+//     } else if (type === "string") {
+//       // Poll results are 13, 2, 4,1
+//       console.log(`Poll results are ${this.answers.join(", ")}`);
+//     }
+//   },
+// };
+// // poll.registerNewAnswer();
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+
+// Immidiately invoked function expression (IIFE) :-
+
+const runOnce = function () {
+  console.log("This will never run again");
 };
-// poll.registerNewAnswer();
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+runOnce();
 
-poll.displayResults.call({ answers: [5, 2, 3] }, "string");
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+// IIFE
+(function () {
+  console.log("This will never run again");
+  const isPrivate = 23;
+})();
+console.log(isPrivate);
+(() => console.log("This will also not run again"))();
