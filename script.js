6b74974db49c085aeefd58ac646aca6f9c247b0d
SuraZz007 <surajdev9886@gmail.com>
2022-07-15 21:52:54 +0530

    JavaScript filter method

--- script.js
+++ script.js
@@ -2026,3 +2026,18 @@ const movementsDes = movements.map(
     )}`
 );
 console.log(movementsDes);
+
+/// The Filter Method
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(` The withdrawls are ${withdrawals}`);
