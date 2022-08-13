a5d29c3dd5d88c7ce9e49b3ed0795ae0f4da6a94
SuraZz007 <surajdev9886@gmail.com>
2022-08-13 18:13:22 +0530

    Assessment related to promises

--- script.js
+++ script.js
@@ -3202,22 +3202,164 @@ const getJSON = function (url, errorMsg = "Something went wrong") {
 //   console.log("3: Finished getting location");
 // })();
 
-const get3Countries = async function (c1, c2, c3) {
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+//     // console.log([data1.capital[0], data2.capital[0], data3.capital[0]]);
+
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
+//     console.log(data.map((d) => d[0].capital[0]));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// get3Countries("portugal", "nepal", "tanzania");
+
+// Promise.race
+// (async function () {
+//   const res = await Promise.race([
+//     getJSON(`https://restcountries.com/v3.1/name/portugal`),
+//     getJSON(`https://restcountries.com/v3.1/name/australia`),
+//     getJSON(`https://restcountries.com/v3.1/name/india`),
+//   ]);
+//   console.log(res[0].name);
+// })();
+
+// const timeOut = function (sec) {
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error("Request took too long!"));
+//     }, sec * 1000);
+//   });
+// };
+// Promise.race([
+//   getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+//   timeOut(5),
+// ])
+//   .then((res) => console.log(res[0]))
+//   .catch((err) => console.error(err));
+
+// //Promise.allSettled
+
+// Promise.allSettled([
+//   Promise.resolve("Success"),
+//   Promise.reject("Error"),
+//   Promise.resolve("Another Success"),
+// ]).then((res) => console.log(res));
+
+// Promise.all([
+//   Promise.resolve("Success"),
+//   Promise.reject("Error"),
+//   Promise.resolve("Another Success"),
+// ])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
+
+// //Promise.any [ES2021]
+// Promise.any([
+//   Promise.resolve("Success"),
+//   Promise.reject("Error"),
+//   Promise.resolve("Another Success"),
+// ])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+GOOD LUCK 😀
+*/
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+let currentImg;
+// createImage("img/img-1.jpg")
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 1 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 2 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//   })
+//   .catch((err) => console.error(err));
+
+const loadNPause = async function () {
   try {
-    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // Load image 1
+    let img = await createImage("img/img-1.jpg");
+    console.log("Image 1 loaded ");
+    await wait(2);
+    img.style.display = "none";
 
-    // console.log([data1.capital[0], data2.capital[0], data3.capital[0]]);
-
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
-    console.log(data.map((d) => d[0].capital[0]));
+    // Load image 1
+    img = await createImage("img/img-2.jpg");
+    console.log("Image 2 loaded ");
+    await wait(2);
+    img.style.display = "none";
   } catch (err) {
     console.error(err);
   }
 };
-get3Countries("portugal", "nepal", "tanzania");
+// loadNPause();
+
+// Part 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async (img) => await createImage(img));
+    console.log(imgs);
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach((img) => img.classList.add("parallel"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"]);
